69683f14366b69e0902fe286672274fb09075a4f
Tsuchiya-Ringtech <tsuchiya.kazunori@ring-tech.jp>
2023-06-08 10:59:07 +0900

    Initial Commit

---
+++ storyboardtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyboardtest
+//
+//  Created by Kazunori Tsuchiya on 2023/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
